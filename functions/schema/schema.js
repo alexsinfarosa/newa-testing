efe0acb57bb75b280bbb117c4c8da0a038efa0ba
alexsinfarosa <as898@cornell.edu>
2019-07-22 15:58:36 +0200

    update graphQL schema

--- functions/schema/schema.js
+++ functions/schema/schema.js
@@ -3,16 +3,38 @@ const { ApolloServer, gql } = require("apollo-server-lambda")
 const typeDefs = gql`
   type Query {
     allUsers: [User!]
-    user(id: Int!): User
-    userByName(name: String!): User
+    userById(id: Int!): User
+    allStations: [Station!]
+    stationById(id: String!): Station
+    allModels: [Model!]
+    modelByName(name: String!): Model
   }
+
   type User {
-    id: ID!
+    id: Int!
     name: String!
     email: String!
     role: String!
-    lat: Int
-    lon: Int
+    lat: Float
+    lon: Float
+    state: String
+    stations: [Station]
+    models: [Model]
+  }
+
+  type Model {
+    name: String!
+    isSeason: Boolean
+  }
+
+  type Station {
+    network: String!
+    lon: Float!
+    state: String!
+    elev: Int
+    lat: Float!
+    id: String!
+    name: String!
   }
 `
 
@@ -22,16 +44,56 @@ const users = [
   { id: 3, name: "Joe", email: "Joe@example.com", role: "user" },
 ]
 
+const models = [
+  { id: 1, name: "Apple Maggot", users: [1, 3] },
+  { id: 2, name: "Cranberry model", users: [1, 2] },
+]
+
+const stations = [
+  {
+    network: "newa",
+    lon: -74.248764,
+    state: "NY",
+    elev: 386,
+    lat: 41.807602,
+    id: "acc",
+    name: "Accord",
+  },
+  {
+    network: "miwx",
+    lon: -85.9605,
+    state: "MI",
+    elev: 935,
+    lat: 43.5544,
+    id: "ew_aet",
+    name: "Aetna/Fremont",
+  },
+]
+
 const resolvers = {
   Query: {
-    user: (root, args, context) => {
-      return
-    },
-    userByName: (root, args, context) => {
-      return users.find(x => x.name === args.name) || "NOTFOUND"
-    },
     allUsers: (root, args, context) => {
       return users
+    },
+    userById: (root, args, context) => {
+      return users.find(d => d.id === args.id) || "NOTFOUND"
+    },
+    allStations: (root, args, context) => {
+      return stations
+    },
+    stationById: (root, args, context) => {
+      return stations.find(d => d.id === args.id) || "NOTFOUND"
+    },
+    allModels: (root, args, context) => {
+      return models
+    },
+    modelByName: (root, args, context) => {
+      return models.find(d => d.name === args.name) || "NOTFOUND"
+    },
+  },
+  Model: {
+    names: (root, args, context) => {
+      return models.filter(m => m)
     },
   },
 }
